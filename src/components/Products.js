063ae6b9e9f9e201d0af78d650940ca93ca49de4
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 20:16:05 +0300

    map

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,13 +6,18 @@ const Products = () => {
     const fetchProducts = async () => {
       const res = await fetch("https://fakestoreapi.com/products");
       const data = await res.json();
+      setProducts(data);
       console.log(data);
     };
     fetchProducts();
   }, []);
   return (
-    <div>
-      <h1>Products</h1>
+    <div className="productWrapper">
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          
+        </div>
+      ))}
     </div>
   );
 };
