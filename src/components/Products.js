e36f7457f2df0ae847ac4ce6c2666dc56dfe0c01
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 20:06:19 +0300

    root

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 const Products = () => {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = res.json();
+      console.log(data);
+    };
+  }, []);
   return (
     <div>
       <h1>Products</h1>
