2cd2d556260549a35726cfd642fe458585a11a06
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 19:41:53 +0300

    link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <span>REDUX STORE</span>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/Card">Card</Link>
+      </div>
     </div>
   );
 };
