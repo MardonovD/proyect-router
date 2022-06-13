8c8aa853dc66168e4076b6ce0d71f5ed0e00da65
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 19:38:07 +0300

    navbar finished

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Card from "./pages/Card";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/card" element={<Card />}></Route>
