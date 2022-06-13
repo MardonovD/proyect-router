3a65befdb6f941962cea696affabf19d89e24782
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-13 19:30:12 +0300

    react router install for project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Card from "./pages/Card";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route to="/" element={<Home />}></Route>
+          <Route to="/card" element={<Card />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
